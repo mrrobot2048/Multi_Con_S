3f6e2ffe0ef6185ae922e4a76e3519d34eb37f0c
mrrobot2048 <53491666+mrrobot2048@users.noreply.github.com>
2019-10-13 20:49:15 -0300

    Esquema básico de un Socket Asincrónico de Clientes multiples...

---
+++ Multi Con S/Multi Con S/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Multi_Con_S
+{
+    class Program
+    {
+        static Listener l;
+        static List<Socket> sockets;
+        static void Main(string[] args)
+        {
+            l = new Listener(8);
+            l.SocketAccepted += L_SocketAccepted;
+            l.Start();
+            sockets = new List<Socket>();
+
+            Console.Read();
+        }
+
+        private static void L_SocketAccepted(Socket e)
+        {
+            Console.WriteLine("New connection. {0}\n{1}\n==============", e.RemoteEndPoint, DateTime.Now);
+            sockets.Add(e);
+        }
+    }
+}
